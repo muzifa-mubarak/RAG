0937c69cc4d6f63e86a45566811791d3ee25700d
muzifa-mubarak <muzifa24@gmail.com>
2025-08-25 23:43:50 +0530

    adding

--- chatbot.py
+++ chatbot.py
@@ -22,7 +22,7 @@ vect=FAISS.from_documents(a,embedd)
 
 model=ChatGoogleGenerativeAI(
     model='gemini-2.5-flash',
-    google_api_key="AIzaSyCFGE7eRBkkPYxTyV3Il5xIRT8a7yOeS2M"
+    google_api_key="your_api"
 )
 
 '''b=input("enter the question :")
